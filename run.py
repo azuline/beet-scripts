400794013cd268ac271414b6295155f17f3e8c1f
blissful <blissful@sunsetglow.net>
2023-01-09 07:41:25 -0500

    fix sub-artist replacements (e.g. x,y;z,a)

--- run.py
+++ run.py
@@ -48,63 +48,64 @@ def sub_artist_tag(tag: str, album: bool) -> list[str]:
 
     cmd_artists = subprocess.run(args, capture_output=True)
     artists = uniq(
-        [
-            y.strip()
-            for x in cmd_artists.stdout.decode().splitlines()
-            if x.strip()
-            for y in re.split(r";(?!\s)", x)
-        ]
+        [x.strip() for x in cmd_artists.stdout.decode().splitlines() if x.strip()]
     )
 
-    commands = []
-    for existing_art in artists:
-        new_art: str = existing_art
+    commands: list[str] = []
+    for full_existing_art in artists:
+        new_artists: list[str] = []
 
-        # BEGIN TRANSFORM
-        new_art = new_art.replace("-", "‐")
-        new_art = new_art.replace(r"&amp;", "&")
-        new_art = re.sub(r"presents", "pres.", new_art, flags=re.IGNORECASE)
-        new_art = re.sub(r"pres\.", "pres.", new_art, flags=re.IGNORECASE)
-        new_art = re.sub(r"featuring", "feat.", new_art, flags=re.IGNORECASE)
-        new_art = re.sub(r"feat\.", "feat.", new_art, flags=re.IGNORECASE)
-        new_art = re.sub(r"feat(?=[^.])", "feat.", new_art, flags=re.IGNORECASE)
+        for existing_part in full_existing_art.split(";"):
+            new_part: str = existing_part
 
-        for splitter in [" & ", ", ", "\\\\", "; ", " / ", " with "]:
-            parts = []
-            to_split = new_art
-            while to_split:
-                for blk in BLACKLIST:
-                    if to_split == blk:
-                        parts.append(blk)
-                        to_split = ""
-                        break
-                    if to_split.startswith(blk) and to_split[len(blk) :].startswith(
-                        splitter
-                    ):
-                        parts.append(blk)
-                        to_split = to_split[len(blk) + len(splitter) :].strip()
-                else:
-                    idx = to_split.find(splitter)
-                    if idx == -1:
-                        parts.append(to_split)
-                        break
-                    parts.append(to_split[:idx])  # type: ignore
-                    to_split = to_split[idx + len(splitter) :].strip()
+            # BEGIN TRANSFORM
+            new_part = new_part.replace("-", "‐")
+            new_part = new_part.replace(r"&amp;", "&")
+            new_part = re.sub(r"presents", "pres.", new_part, flags=re.IGNORECASE)
+            new_part = re.sub(r"pres\.", "pres.", new_part, flags=re.IGNORECASE)
+            new_part = re.sub(r"featuring", "feat.", new_part, flags=re.IGNORECASE)
+            new_part = re.sub(r"feat\.", "feat.", new_part, flags=re.IGNORECASE)
+            new_part = re.sub(r"feat(?=[^.])", "feat.", new_part, flags=re.IGNORECASE)
 
-            new_art = ";".join(parts)
-        # END TRANSFORM
+            for splitter in [" & ", ", ", "\\\\", "; ", " / ", " with "]:
+                parts = []
+                to_split = new_part
+                while to_split:
+                    for blk in BLACKLIST:
+                        if to_split == blk:
+                            parts.append(blk)
+                            to_split = ""
+                            break
+                        if to_split.startswith(blk) and to_split[len(blk) :].startswith(
+                            splitter
+                        ):
+                            parts.append(blk)
+                            to_split = to_split[len(blk) + len(splitter) :].strip()
+                    else:
+                        idx = to_split.find(splitter)
+                        if idx == -1:
+                            parts.append(to_split)
+                            break
+                        parts.append(to_split[:idx])  # type: ignore
+                        to_split = to_split[idx + len(splitter) :].strip()
 
-        for k, v in ALIASES.items():
-            k = f"(^|;){re.escape(k)}(;|$)"
-            new_art = re.sub(k, v, new_art)
+                new_part = ";".join(parts)
+            # END TRANSFORM
 
-        if existing_art == new_art:
+            for k, v in ALIASES.items():
+                k = f"(^|;){re.escape(k)}(;|$)"
+                new_part = re.sub(k, v, new_part)
+
+            new_artists.append(new_part)
+
+        combined_new_art = ";".join(new_artists)
+        if full_existing_art == combined_new_art:
             continue
 
         album_flag = "-a " if album else ""
         commands.append(
             f"beet modify --yes {album_flag}"
-            f"{tag}::'^{re.escape(existing_art)}$' {tag}='{new_art}'"
+            f"{tag}::'^{re.escape(full_existing_art)}$' {tag}='{combined_new_art}'"
         )
 
     return commands
